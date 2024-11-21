5e8a3df2997c4636d2c16eea1157ed378e8124ff
ezhilgreensomr <ezhilgreensomr@gmail.com>
2024-11-21 11:46:52 +0530

    1st commit

---
+++ src/com/individuallogin/CreateNewAccount.java
@@ -0,0 +1,31 @@
+package com.individuallogin;
+
+public class CreateNewAccount {  //class
+	
+	//Method 1
+	private void emailId() {
+		
+		System.out.println("greensavadi@gmail.com");
+	}
+	//Method 2
+	private void password() {
+		
+		System.out.println("1234567890");
+	}
+	//Method 3
+	private void phoneNumber() {
+	
+		System.out.println("8493849384");
+	}
+	//Main Method
+	public static void main(String[] args) {
+
+		//Object Creation -- ClassName objectName = new ClassName();
+		CreateNewAccount c = new CreateNewAccount();
+		
+		//Method call - objectName.methodName();
+		c.emailId();
+		c.password();
+		c.phoneNumber();
+	}
+}
